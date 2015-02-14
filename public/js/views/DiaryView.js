f44fb69edefc4f0c71580f78fcb16557b67e163c
nameofname <nameofemail@gmail.com>
2015-02-14 13:22:51 -0500

    Added some code to filter and sort times.

--- public/js/views/DiaryView.js
+++ public/js/views/DiaryView.js
@@ -31,7 +31,11 @@
 
     });
 
-
+    /**
+     * Row view represents each day model. For each day you can click on the time slots to indicate your sleep patterns.
+     * This view has as it's sub views a collection of DiaryTimeViews (below)
+     * @type {void|*}
+     */
     app.DiaryRowView = BBC.BaseView.extend({
 
         className : 'diary-row',
@@ -50,14 +54,32 @@
         },
 
         renderTimes : function () {
-            var self = this;
             var json = this.model.toJSON();
+            var day = [];
+            var night = [];
 
-            _.each(json, function (val, key) {
-                if (self._isTimeKey(key)) {
-                    self._addTime(val, key);
-                }
-            });
+            // TODO ::: THIS LOGIC GETS ME THE VALUES OUT OF ORDER AND SLIGHTLY WRONG... FIX IT.
+
+            var morning = this._filterTimes(true, 8, 12);
+            var noon = this._filterTimes(false, 1, 8);
+            // TODO : !!!!!!!
+
+            // First add the divider with the "Day" label
+            // Next loop over the day values and add each cell :
+            // Now add another divider with the "Night" label
+            // Finally add each of the night cells :
+            this._addDivider('Day :');
+
+            _.each(day, function (time) {
+                this._addTime(this.model.get(time), time);
+            }, this);
+
+            this._addDivider('Night :');
+
+            _.each(night, function (time) {
+                this._addTime(this.model.get(time), time);
+            }, this);
+
         },
 
         changeSleepState : function (time, state) {
@@ -65,30 +87,56 @@
             this.model.save();
         },
 
-        _addTime : function (val, key) {
+        _addDivider : function(label) {
+            this._currContainer = $('<div>').addClass('time-container');
+            this._currContainer.prepend($('<div>').addClass('time-ampm').html(label));
+            this.$el.append(this._currContainer);
+        },
+
+        _addTime : function (val, fullTime) {
             var currLen = this.subViews.length;
             var time = '';
 
-            if (!this._currContainer || currLen === 48) {
-                this._currContainer = $('<div>').addClass('time-container');
-                this._currContainer.prepend($('<div>').addClass('time-ampm').html(this._amPm));
-                this._amPm = 'PM : ';
-                this.$el.append(this._currContainer);
-            }
-
             if (currLen % 4 === 0) {
-                time = key.split(':')[0];
+                time = fullTime.split(':')[0];
             }
 
             var timeView = this.subViews.add(app.DiaryTimeView, {
                 model : new Backbone.Model({
                     displayTime : time,
-                    time : key,
+                    time : fullTime,
                     state : val
                 })
             }).render();
 
             this._currContainer.append(timeView.$el);
+        },
+
+        /**
+         * TODO: Make this function take the upper, lower, and am distinction and apply .
+         * @param am
+         * @param lowerBound
+         * @param upperBound
+         * @private
+         */
+        _filterTimes : function (am, lowerBound, upperBound) {
+            var json = this.model.toJSON();
+            _.each(json, function (val, timeKey) {
+
+                if (this._isTimeKey(timeKey)) {
+                    var hour = parseInt(timeKey.split(':')[0], 10);
+                    var am = timeKey.match(/[a-z]{2}/)[0] === 'am';
+                    var morning = ((8 <= hour) && (hour <= 12)) && am;
+                    var noon = (hour === 12 || ((1 <= hour) && ( hour <= 8))) && !am;
+
+                    if (morning || noon) {
+                        day.push(timeKey);
+                    } else {
+                        night.push(timeKey);
+                    }
+                }
+
+            }, this);
         },
 
         _isTimeKey : function (str) {
